d0050a1b43bd0ee7f628e7bad518bcfd872675a5
RainbowKatz <tylerjkatz@icloud.com>
2018-10-28 03:24:38 -0400

    Add initial version of tabs package with SplitDiff func and tests

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# go-split-money
-Quick Go demo for splitting money with a predetermined difference.
+<h1>go-split-money</h1>
+
+An example project that holds a ```go get```able package to split a tab into shares.
+
+<h2>Table of Contents</h2>
+
+- [Package List](#package-list)
+    - [tabs](#tabs)
+
+
+# tabs
+## GoDoc
+For documentation, see [GoDoc](https://godoc.org/github.com/RainbowKatz/go-split-money/tabs)
+
+## How to Run
+The ```main.go``` file is set to run ```SplitDiffPrint```, a convenience func which calls the ```SplitDiff``` function, and then displays the formatted results.
+
+To run with default values (from repo root):
+```
+$ go run main.go
+```
+output:
+```
+SplitDiff(tab=10.000000, shares=2, diff=1.000000) >>
+  Share 1: $4.50
+  Share 2: $5.50
+```
+
+To run with custom values, use command-line flags:
+
+```
+$ go run main.go -tab=28.00 -shares=5 -diff=2.00
+
+SplitDiff(tab=28.000000, shares=5, diff=2.000000) >>
+  Share 1: $1.60
+  Share 2: $3.60
+  Share 3: $5.60
+  Share 4: $7.60
+  Share 5: $9.60
+```
+
+Testing a case with rounding error (taken from [test cases](./tabs/splitdiff_test.go))
+```
+$ go run main.go -tab=7 -shares=2 -diff=2.33
+
+SplitDiff(tab=7.000000, shares=2, diff=2.330000) >>
+  Share 1: $2.34
+  Share 2: $4.67
+
+Error: Results not exact due to rounding to nearest cent
+exit status 1
+```
---
+++ tabs/splitdiff.go
@@ -0,0 +1,100 @@
+/*
+	Package Tabs provides methods of splitting a typical tab between multiple parties.
+*/
+package tabs
+
+import (
+	"fmt"
+	"math"
+)
+
+const stdErr float64 = 0.009
+
+// SplitDiffPrint calls SplitDiff and then prints the results
+func SplitDiffPrint(tab float64, shares int, diff float64) ([]float64, error) {
+	results, err := SplitDiff(tab, shares, diff)
+	if err != nil && results == nil {
+		return nil, err
+	}
+
+	//display results
+	fmt.Printf("\nSplitDiff(tab=%f, shares=%d, diff=%f) >>\n", tab, shares, diff)
+	for i, share := range results {
+		fmt.Printf("  Share %d: $%.2f\n", i+1, share)
+	}
+
+	// return results
+	return results, err
+}
+
+// SplitDiff splits a tab into a given number of shares, with a diff between each consecutive share.
+// SplitDiff may return results along with an error if rounding error(s) occurred.
+func SplitDiff(tab float64, shares int, diff float64) ([]float64, error) {
+	// output
+	var splitTabs []float64
+	var shareTotal float64
+
+	// validate
+	if err := validateSplitDiff(tab, shares, diff); err != nil {
+		return nil, err
+	}
+
+	// calculate first/lowest share
+	var lowestShare float64
+	if shares%2 == 0 {
+		lowestShare = tab/float64(shares) - (diff/float64(2))*(2*float64(shares/2)-1)
+	} else {
+		lowestShare = tab/float64(shares) - float64(shares/2)*diff
+	}
+
+	for w, currentShare := 0, 0.; w < shares; w++ {
+		currentShare = lowestShare + float64(w)*diff
+
+		// rount to nearest penny
+		currentShare = math.Round(currentShare/0.01) * 0.01
+		splitTabs = append(splitTabs, currentShare)
+		shareTotal += currentShare
+	}
+
+	// if split not possible due to rounding to nearest cent, return results anyway along with error
+	if math.Abs(shareTotal-tab) >= stdErr {
+		return splitTabs, fmt.Errorf("Results not exact due to rounding to nearest cent")
+	}
+
+	// return output with no error
+	return splitTabs, nil
+}
+
+// validateSplitDiff performs validation for the SplitDiff function
+func validateSplitDiff(tab float64, shares int, diff float64) error {
+	// validate total, must be positive
+	if tab <= 0. {
+		return fmt.Errorf("tab must be positive")
+	}
+
+	// validate shares is at least 2
+	if shares < 2 {
+		return fmt.Errorf("shares must be at least 2")
+	}
+
+	// validate diff, must be non-negative
+	if diff < 0. {
+		return fmt.Errorf("diff must be non-negative")
+	}
+
+	// calculate the minLargestShare, minimum largest share to satisfy the diff and number of shares
+	//   i.e. 4 shares and diff of 2 means minLargestShare of 6 >> 0, 2, 4, 6
+	minLargestShare := float64(shares-1) * diff
+
+	// calculate minTab, that is the minimum tab to perform the split given the diffs
+	minTab := float64(shares/2) * minLargestShare
+	// 1st term is 0, last is minLargestShare, formula @ http://www.mathwords.com/a/arithmetic_series.htm
+
+	// validate input tab is not less than minTab
+	if tab < minTab {
+		return fmt.Errorf("cannot split tab %f into %d tabs with a diff of %f between each tab", tab, shares, diff)
+	}
+
+	// validated
+	return nil
+}
